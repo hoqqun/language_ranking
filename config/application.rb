d1e50fe67b584c98c22fe2c1ebb03f635ba3bda6
sierra <sierra@usernoMacBook-Pro.local>
2018-06-12 02:20:43 +0900

    application.rb変更

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module LanguageRanking
     config.load_defaults 5.2
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
